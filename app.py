1dfee8d463ad2c81e13eaf887dcf57058900dc3c
Zachary Buchanan <zach@Zacharys-MBP.attlocal.net>
2023-09-07 22:54:35 -0500

    gunicorn

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-from pokemon import pokemon, abilities, moves, types
+from pkmnLists import pokemon, abilities, moves, types
+import gunicorn.app.base
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'yurr'
-debug = DebugToolbarExtension(app)
+# app.config['SECRET_KEY'] = 'yurr'
+# debug = DebugToolbarExtension(app)
+class StandaloneApplication(gunicorn.app.base.BaseApplication):
+    def __init__(self, app, options=None):
+        self.options = options or {}
+        self.application = app
+        super().__init__()
+
+    def load_config(self):
+        for key, value in self.options.items():
+            if key in self.cfg.settings and value is not None:
+                self.cfg.set(key, value)
+
+    def load(self):
+        return self.application
+
 
 @app.route('/')
 def start():
