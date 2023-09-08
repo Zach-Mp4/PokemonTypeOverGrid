06e91cb2d6e2164e2b4e175d031570ae21d7f95e
Zachary Buchanan <zach@Zacharys-MBP.attlocal.net>
2023-09-07 22:43:16 -0500

    started python

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-
+from pokemon import pokemon, abilities, moves, types
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'yurr'
